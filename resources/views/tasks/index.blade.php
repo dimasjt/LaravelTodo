1f6fa285e042c162d09e66583c95355bab6741a0
Dimas J. Taniawan <dimazniawan@gmail.com>
2016-04-30 17:02:03 +0700

    change btn size

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -58,7 +58,7 @@
                                             {!! csrf_field() !!}
                                             {!! method_field('DELETE') !!}
 
-                                            <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
+                                            <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger btn-xs">
                                                 <i class="fa fa-btn fa-trash"></i>Delete
                                             </button>
                                         </form>
